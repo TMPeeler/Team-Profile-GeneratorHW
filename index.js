790809e503d19d974148e2ee67d793624f64909e
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-18 01:18:42 -0700

    add starter files and function skeleton

---
+++ index.js
@@ -0,0 +1,36 @@
+//dependencies: inquirer, FS, tests?
+//inquirer prompt for user input 
+// user input creates team manager object with name, employee ID, email, and office number.
+// user input creates engineer object with name, ID, email, and github username.
+// user input creates intern object with name, ID, email, and school.
+
+const fs = require('fs');
+const inquirer = require('inquirer');
+const manager = require('./manager.js');
+const engineer = require('./engineer.js');
+const intern = require('./intern.js');
+
+
+genManager() {
+// this is a manager only
+// use inquirer prompt
+
+}
+
+genEmployee() {
+//this is either an engineer or an intern
+// use inquirer prompt to choose either occupation and then depending on the one chosen, execute generator function for that one
+
+}
+
+genEngineer() {
+
+
+}
+
+
+genIntern() {
+
+
+}
+
