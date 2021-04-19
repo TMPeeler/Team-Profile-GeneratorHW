698467dae611e6a158d81d1710624dfda7cd98ae
Thomas Peeler <Thomasmwpeeler@Gmail.com>
2021-04-18 21:35:26 -0700

    add inquire prompts for each team member

--- index.js
+++ index.js
@@ -18,56 +18,112 @@ function genManager() {
  return inquirer.prompt([
      
     {
-    type: 'input', 
-    message: 'Please enter the name of your project manager: ',
-    name: 'name'
+        type: 'input', 
+        message: 'Please enter the name of your project manager: ',
+        name: 'name'
     },
-
     {
         type: 'input', 
         message: "Please enter your manager's employee ID: ",
         name: 'id'
     },
-
     {
         type: 'input', 
         message: "Please enter your manager's email address: ",
         name: 'email'
     },
-
     {
         type: 'input', 
         message: "Please enter your manager's office number: ",
         name: 'office'
     }
- ]).then((response)) => {
-    let managerText =
-
-
+ ]).then((response) => {
+    let managerText = ``;
  }
- 
- 
- 
- ;
-
+ );
 }
 
 
-fuction genEmployee() {
+function genEmployee() {
 //this is either an engineer or an intern
 // use inquirer prompt to choose either occupation and then depending on the one chosen, execute generator function for that one
+return inquirer.prompt([
+
+    {
+        type: 'input', 
+        message: 'Please choose to add one of the following to your team: ',
+        choices: [
+            'Engineer',
+            'Intern'
+        ]
+        },
+
+    ]).then((response) => {
+        if(response === 'Engineer') {
+            genEngineer();
+
+        }else if (response === 'Intern') {
+            genIntern();
+        }
+    })
+}
+
+function genEngineer() {
+    return inquirer.prompt([
+     
+        {
+            type: 'input', 
+            message: 'Please enter the name of your engineer: ',
+            name: 'name'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your engineer's employee ID: ",
+            name: 'id'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your engineer's email address: ",
+            name: 'email'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your engineer's github username: ",
+            name: 'github'
+        }
+    ]).then((response) => {
+        let engineerText = ``;
+    });
 
 }
 
-genEngineer() {
 
-
-}
-
-
-genIntern() {
-
-
+function genIntern() {
+    return inquirer.prompt([
+     
+        {
+            type: 'input', 
+            message: 'Please enter the name of your intern: ',
+            name: 'name'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your intern's employee ID: ",
+            name: 'id'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your intern's email address: ",
+            name: 'email'
+        },
+        {
+            type: 'input', 
+            message: "Please enter your intern's school: ",
+            name: 'school'
+        }
+    ]).then((response) => {
+        let engineerText = ``;
+    });
 }
 
 // I need objects to be created via inquire prompts
